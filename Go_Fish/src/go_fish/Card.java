6afa0281cacdcdade3f1e238cee020827b53a4ba
Yuvraj <officialyuvrajs@gmail.com>
2024-10-10 21:51:36 -0400

    created Card class and made two private strings rank and suit.

---
+++ Go_Fish/src/go_fish/Card.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package go_fish;
+
+/**
+ *
+ * @author rav0073
+ */
+public class Card {
+    private String rank;
+    private String suit;
+}
