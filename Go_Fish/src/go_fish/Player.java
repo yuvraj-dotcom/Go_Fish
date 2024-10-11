16d5cf88aa6d0d5718a50ce58cc1d95cbb0d43aa
Krthk0 <sreenina@sheridancollege.ca>
2024-10-10 22:26:35 -0400

    Update Player.java
    player class changed

--- Go_Fish/src/go_fish/Player.java
+++ Go_Fish/src/go_fish/Player.java
@@ -9,5 +9,37 @@ package go_fish;
  * @author rav0073
  */
 class Player {
+    private string name;
+    private List<Card> hand;
     
-}
+    publicPlayer(String name){
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+    public void addCardToHand(Card card){
+        hand.add(card);
+    }
+    public Card askforCard(String rank) {
+        for(Card card : hand) {
+            if (card.getrank().equals(rank)){
+                return card;
+            }
+        }
+        return null;
+    }
+    public List<Card> getHAnd() {
+        return hand;
+    }
+    public String getName() {
+        return name;
+    }
+    public void removeCard(Card card){
+        hand.remove(card);
+    }
+    @Override
+    public String toString() {
+        return name + " 's hand: "+ hand;
+    }
+    
+    
+
