2024b62bb71f12e6474510b28ea5cf80b4d42a86
Yuvraj <officialyuvrajs@gmail.com>
2024-10-10 22:47:29 -0400

    fixed Deck class according to other classes

--- Go_Fish/src/go_fish/Deck.java
+++ Go_Fish/src/go_fish/Deck.java
@@ -8,13 +8,13 @@ package go_fish;
  *
  * @author rav0073
  */
-class Deck {
-    import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 public class Deck{
     private List<Card> cards;
-    publicDeck() {
+    
+    public Deck() {
         cards = new ArrayList<>();
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack","Queen","King","Ace",};
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades",};
@@ -25,7 +25,7 @@ public class Deck{
             }
         }
     }
-    public void shufffle() {
+    public void shuffle() {
         Collections.shuffle(cards);
     }
     public Card drawCard() {
