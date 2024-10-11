0c1b5730257ac103f78b99d29eeb3f86790971b5
Yuvraj <officialyuvrajs@gmail.com>
2024-10-10 22:27:17 -0400

    completed main class

--- Go_Fish/src/go_fish/Go_Fish.java
+++ Go_Fish/src/go_fish/Go_Fish.java
@@ -1,3 +1,4 @@
+package go_fish;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,7 +14,8 @@ public class Go_Fish {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game goFish = new Game(2);
+        goFish.startGame();
     }
     
 }
