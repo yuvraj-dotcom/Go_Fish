5b72fc276bf6e2822741048d446e9836d04d5e72
Yuvraj <officialyuvrajs@gmail.com>
2024-10-10 21:56:22 -0400

    created default constructor and getters for the Strings.

--- Go_Fish/src/go_fish/Card.java
+++ Go_Fish/src/go_fish/Card.java
@@ -11,4 +11,22 @@ package go_fish;
 public class Card {
     private String rank;
     private String suit;
+    
+    public Card(String rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+    
+    public String getRank(){
+        return rank;
+    }
+    
+    public String getSuit(){
+        return suit;
+    }
+    
+    @Override
+    public String toString(){
+        return rank + " of " + suit;
+    }
 }
